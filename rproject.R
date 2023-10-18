9c6b8f76ce8954402f34f1c8d101885e3aa51ddc
Prajith J Shetty <prajithjshetty1@gmail.com>
2023-10-18 23:05:19 +0530

    Added score of team permatch

--- rproject.R
+++ rproject.R
@@ -12,5 +12,19 @@ for(i in unique_values ){
   j=j+1
 }
 print("Teams And Their Total Scores ")
+result <- aggregate(Goals ~ Team, Data, sum)
+print(result)
+print("Teams and their scores on average per match")
+j=1
+for(i in unique_values ){
+  team_to_calculate <- i
+  team_data <- df[Data$Team == team_to_calculate, ]
+  mean_score <- mean(team_data$Goals)
+  cat(j,i,mean_score)
+  j=j+1
+}
+result <- aggregate(Goals ~ Team, Data, mean)
+print(result)
 
-
+final_match=max(Data$Date)
+final_match
