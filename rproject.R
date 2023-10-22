ae50c1fe220dcb86ba03856594762f17d481302e
Prajith J Shetty <prajithjshetty1@gmail.com>
2023-10-22 15:56:41 +0530

    added ggplot

--- rproject.R
+++ rproject.R
@@ -36,3 +36,18 @@ dev.off()
 png(file="barfootball.png")
 barplot(x,names.arg = lbl,xlab = "Teams",ylab = "Goals",col = "blue",border = "red",main ="Bar plot of football teams and their total score")
 dev.off()
+
+
+
+library(dplyr)
+library(readxl)
+Data <- read_excel("Premier League 2011-12 Match by Match.xls")
+aggregated_data <- Data %>% group_by(Team) %>% summarise(Goals = sum(Goals))
+p <- ggplot(aggregated_data, aes(x = Team, y = Goals)) +
+  geom_bar(stat = "identity", fill = "black") +
+  geom_text(aes(label = lbl, y = x/2), size = 4, angle = 90,vjust=0.3, color = "white") +
+  theme_minimal() +
+  theme(axis.text.x = element_blank()) +
+  labs(x = "Team", y = "Goals")
+print(p)
+
