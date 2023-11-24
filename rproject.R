d1cb45c9054609b9480ca5f85df14ea042fb7650
Prajith J Shetty <prajithjshetty1@gmail.com>
2023-11-24 07:57:37 +0530

    Added prediction of cleansheets and the team which won fairplay award

--- rproject.R
+++ rproject.R
@@ -14,26 +14,29 @@ for(i in unique_values ){
 }
 ###################################################################################
 #2.Get teams with their total score in that season
-print("Teams And Their Total Scores ")
+
 result <- aggregate(Goals ~ Team, Data, sum)
+print("Teams And Their Total Scores ")
 print(result)
 ###############################################################################
 #3.Get Teams with their scores on average per match
-print("Teams and their scores on average per match")
+
 
 result2 <- aggregate(Goals ~ Team, Data, mean)
+print("Teams and their scores on average per match")
 print(result2)
 ########################################################################################################
 #4.Bar graph teams and their total score in the season
 library(dplyr)
 library(readxl)
+library(ggplot2)
 Data <- read_excel("Premier League 2011-12 Match by Match.xls")
 aggregated_data <- Data %>% 
   group_by(Team) %>% 
   summarise(Goals = sum(Goals))
 p <- ggplot(aggregated_data, aes(x = Team, y = Goals)) +
   geom_bar(stat = "identity", fill = "black") +
-  geom_text(aes(label = Team, y = Goals/2), size = 4, angle = 90,vjust=0.3, color = "white") +
+  geom_text(aes(label = Team, y = Goals/2), size = 4, angle = 90,vjust=1, color = "white") +
   theme_minimal() +
   theme(axis.text.x = element_blank()) +
   labs(x = "Team", y = "Goals")
@@ -62,7 +65,86 @@ if (nrow(filtered_data) == 0) {
 user_date <- as.Date(readline("Enter a date (yyyy-mm-dd): "))
 check_d(user_date)
 
+#6.Team Which Won Fairplay Award
+
+df <- data.frame(
+  Team = Data$Team,
+  Yellow_Cards = Data$`Yellow Cards`,
+  Red_Cards = Data$`Red Cards`
+
+)
+df <- df %>%
+  mutate(Total_Cards = Yellow_Cards + Red_Cards)
+fair_play_winner <- df %>%
+  group_by(Team) %>%
+  summarise(Total_Cards = sum(Total_Cards)) %>%
+  arrange(Total_Cards) %>%
+  slice(1)
+print("The Team Which Won Fairplay Award")
+print(fair_play_winner)
+
+#7.Clean Sheet Prediction for goal keepers
+# Load necessary libraries
+library(readxl)
+library(dplyr)
+library(caret)  # For modeling
+
+# Read data from Excel file
+file_path <- "Premier League 2011-12 Match by Match.xls"
+data <- read_excel(file_path)  # Replace "Sheet1" with the correct sheet name
+
+# Select relevant columns for goalkeeper analysis and filter rows
+goalkeeper_data <- data %>%
+  select(
+    'Player Surname', 'Player Forename', 'Saves Made', 'Goals Conceded',
+    Catches, Punches, 'Clean Sheets'
+  ) %>%
+  filter(`Saves Made` != 0)  # Filtering out rows where Saves Made is not equal to zero
+
+# Split the data into features and target variable
+features <- goalkeeper_data %>%
+  select(-`Clean Sheets`)  # Excluding the target variable
+target <- goalkeeper_data$`Clean Sheets`
 
 
+# Split the data into training and testing sets
+set.seed(123)  # For reproducibility
+train_index <- createDataPartition(target, p = 0.8, list = FALSE)
+train_data <- features[train_index, ]
+test_data <- features[-train_index, ]
+train_target <- target[train_index]
+test_target <- target[-train_index]
 
+# Train a linear regression model
+model <- train(
+  x = train_data,
+  y = train_target,
+  method = "lm"  # Using linear regression
+)
+
+# Predict on the test set
+predictions <- predict(model, test_data)
+goalkeeper_data$`Player Surname` <- as.character(goalkeeper_data$`Player Surname`)
+
+# New goalkeeper data for prediction
+new_goalkeeper_data <- data.frame(
+  'Saves Made' = 6,
+  'Goals Conceded' = 0,
+  Catches = 1,
+  Punches = 2,
+  'Player Surname' = 'Hart',
+  `Player Forename` = "Joe" 
+)
+# Rename the column 'Player.Surname' to 'Player Surname'
+# Rename the columns to match the model's training columns
+names(new_goalkeeper_data)[names(new_goalkeeper_data) == "Player.Surname"] <- "Player Surname"
+names(new_goalkeeper_data)[names(new_goalkeeper_data) == "Player.Forename"] <- "Player Forename"
+names(new_goalkeeper_data)[names(new_goalkeeper_data) == "Saves.Made"] <- "Saves Made"
+names(new_goalkeeper_data)[names(new_goalkeeper_data) == "Goals.Conceded"] <- "Goals Conceded"
+
+# Predict using the trained model
+predicted_clean_sheets <- predict(model, newdata = new_goalkeeper_data)
+print(predicted_clean_sheets)
+
+str(new_goalkeeper_data)
 
